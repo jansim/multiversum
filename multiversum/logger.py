6d5096625a163c158c63965409c05fb2f6fe253a
Jan Simson <git@simson.io>
2024-11-08 17:18:35 +0100

    Configure logger to print INFO level by default

--- multiversum/logger.py
+++ multiversum/logger.py
@@ -1,5 +1,10 @@
 import logging
+import sys
 
 logger = logging.getLogger("multiversum")
-# Default to only showing INFO messages (and above)
 logger.setLevel(logging.INFO)
+
+if not logger.hasHandlers():
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)

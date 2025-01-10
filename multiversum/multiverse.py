022bb49784f2081b23d9ef85419f0b8bdf2e9486
Jan Simson <git@simson.io>
2025-01-10 12:46:42 +1100

    Fix formatting issues

--- multiversum/multiverse.py
+++ multiversum/multiverse.py
@@ -344,7 +344,9 @@ class MultiverseAnalysis:
         # Clean up any old error fiels
         error_path = self._get_error_filepath(universe_id)
         if error_path.is_file():
-            warnings.warn(f"Removing old error file: {error_path}. This should only happen during a re-run.")
+            warnings.warn(
+                f"Removing old error file: {error_path}. This should only happen during a re-run."
+            )
             error_path.unlink()
 
         # Generate final command
